8944c135eb70472936325b6f79dbc03b3ab84ae3
peter <peter.k.nets@gmail.com>
2015-11-25 10:40:51 +0200

    Finish category sorting and add global UL styling

--- app/js/directives/generic-list.js
+++ app/js/directives/generic-list.js
@@ -9,7 +9,8 @@ angular.module("darksideApp")
       scope: {
         list: "=",
         search: "=",
-        activeTime: "="
+        activeTime: "=",
+        activeLocation: "="
       }
     }
   });
